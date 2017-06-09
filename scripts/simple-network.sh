b3c2af59e70c147dd99caee38f70eb6c758923ae
Antoine Tenart <antoine.tenart@free-electrons.com>
2017-06-09 16:43:55 +0200

    scripts: simplet-network: use a dhcp client
    Signed-off-by: Antoine Tenart <antoine.tenart@free-electrons.com>

--- scripts/simple-network.sh
+++ scripts/simple-network.sh
@@ -2,15 +2,26 @@
 
 echo "#### Beginning simple network board test ####"
 
-NH=192.168.2.1
+iface="eth0"
+case $1 in
+    "armada-7040-db")
+        iface="eth1"
+	;;
+esac
 
 if [ $(ip l | wc -l) -le 2 ]; then
     echo "Only the loopback interface is registered"
     exit 1
 fi
 
-if ! ping -c 4 $NH; then
-    echo "Cannot ping $NH. Aborting."
+if ! dhclient $iface; then
+    echo "Cannot obtain a lease. Aborting."
+    exit 1
+fi
+
+nh=192.168.2.1
+if ! ping -c 4 $nh; then
+    echo "Cannot ping $nh. Aborting."
     exit 1
 fi
 echo "Ping OK."
